40deefd35aefa5732bc90b503a22161c8b353e4f
jimwang <1163747770@qq.com>
2020-12-07 21:12:26 +0000

    update 20.12.07

---
+++ solutions/S0078.java
@@ -0,0 +1,29 @@
+package solutions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 78. 子集 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+ * 
+ * 说明：解集不能包含重复的子集。
+ * 
+ * 示例:
+ * 
+ * 输入: nums = [1,2,3] 输出: [ [3], [1], [2], [1,2,3], [1,3], [2,3], [1,2], [] ]
+ */
+public class S0078 {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+        for (int i = 0; i < nums.length; i++) {
+           int all = res.size();
+            for (int j = 0; j < all; j++) {
+                List<Integer> tmp = new ArrayList<>(res.get(j));
+                tmp.add(nums[i]);
+                res.add(tmp);
+            }
+        }
+        return res;
+    }
+}
