3e085a8016e9e53f3a7a64fe9f143aff4a751533
jimwang <1163747770@qq.com>
2021-01-18 20:56:57 +0000

    update 20.01.18(1)

--- solutions/S0787^.java
+++ solutions/S0787^.java
@@ -1,5 +1,7 @@
 package solutions;
 
+import java.util.Arrays;
+
 /**
  * 787. K 站中转内最便宜的航班
 有 n 个城市通过 m 个航班连接。每个航班都从城市 u 开始，以价格 w 抵达 v。
@@ -23,31 +25,26 @@ https://leetcode-cn.com/problems/cheapest-flights-within-k-stops/solution/k-zhan
  */
 public class S0787 {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
-        int[][][] dp = new int[n][n][K+1];
-        for(int[] f : flights){
-            dp[f[0]][f[1]][0] = f[2];
+        int[][] dp = new int [n][K+1];
+
+        for(int i = 0;i<n;i++) Arrays.fill(dp[i], Integer.MAX_VALUE);
+        for(int[] f : flights) {
+            if(f[0]==src){
+                dp[f[1]][0] = f[2];
+            }
         }
-        for(int k = 1; k <= K; k++){
-            for(int i = 0;i < n; i++){ //起点
-                for(int j = 0; j < n; j++) { //中专点
-                    if(dp[i][j][k-1]!=0) {
-                        for(int z = 0; z<n;z++){
-                            if(dp[j][z][k-1]!=0) {
-                                if(dp[i][z][k]==0){
-                                    dp[i][z][k] = dp[i][j][k-1] +dp[j][z][k-1];
-                                } else {
-                                    dp[i][z][k] = Math.min(dp[i][j][k-1] +dp[j][z][k-1], dp[i][z][k]);
-                                }
-                            }
-                        }
-                    }
+
+        for(int i = 0;i<=K;i++){
+            dp[src][i] = 0;
+        }
+
+        for(int k=1;k<=K;k++){
+            for(int[] f : flights) {
+                if(dp[f[0]][k-1]!=Integer.MAX_VALUE) {
+                    dp[f[1]][k] = Math.min(dp[f[1]][k], dp[f[0]][k-1]+f[2]);
                 }
             }
         }
-        int ans = Integer.MAX_VALUE;
-        for(int k = 0; k<=K; k++){
-            if(dp[src][dst][k]!=0) ans = Math.min(ans, dp[src][dst][k]);
-        }
-        return dp[src][dst][K]==Integer.MAX_VALUE?-1:dp[src][dst][K];
+        return dp[dst][K] == Integer.MAX_VALUE ? -1 : dp[dst][K];
     }
 }
