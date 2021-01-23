90f9e6697671e63567ec6efb3ff2d34fa9c69e35
jimwang <1163747770@qq.com>
2021-01-23 21:49:00 +0000

    update 21.01.23(2)

---
+++ solutions/S0005^.java
@@ -0,0 +1,40 @@
+package solutions;
+
+/**
+ * 5. 最长回文子串
+给你一个字符串 s，找到 s 中最长的回文子串。
+
+ 
+
+示例 1：
+
+输入：s = "babad"
+输出："bab"
+解释："aba" 同样是符合题意的答案。
+ */
+public class S0005 {
+    public String longestPalindrome(String s) {
+        int len = s.length();
+        boolean[][] dp = new boolean[len][len];
+        int maxLen = 1;
+        int begin = 0;
+        for(int j = 0; j < len; j++){
+            for(int i = 0; i < j; i++) {
+                if(s.charAt(i) != s.charAt(j)) {
+                    dp[i][j] = false;
+                } else {
+                    if(j-i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i+1][j-1];
+                    }
+                }
+                if(dp[i][j] && j-i+1>maxLen) {
+                    maxLen = j-i+1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin+maxLen);
+    }
+}
